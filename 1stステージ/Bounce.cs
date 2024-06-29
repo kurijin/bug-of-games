2208c9513b9f84c853d448c15525b6598b608c63
kurijin <144934328+kurijin@users.noreply.github.com>
2024-06-29 20:20:59 +0900

    Update Bounce.cs

--- 1stステージ/Bounce.cs
+++ 1stステージ/Bounce.cs
@@ -4,26 +4,24 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    public float force = 10f; // Force 10000f
-    public float stunTime = 0.5f;
+    public float force = 10f; //　playerを押す力
+    public float stunTime = 0.5f; //playerの硬直時間
     private Vector3 hitDir;
 
+    
     void OnCollisionEnter(Collision collision)
     {
         foreach (ContactPoint contact in collision.contacts)
         {
-            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            //ColiisionのtagがPlayerであった場合の処理
             if (collision.gameObject.tag == "Player")
             {
                 hitDir = contact.normal;
                 var playerController = collision.gameObject.GetComponent<PlayerController>();
                 if (playerController != null)
                 {
+                   //Playerのスクリプトに引き渡すもの.ヒット距離に力をかけて,stan時間も変数に渡す。
                     playerController.HitPlayer(-hitDir * force, stunTime);
-                }
-                else
-                {
-                    Debug.LogError("PlayerController component not found on Player object.");
                 }
                 return;
             }
